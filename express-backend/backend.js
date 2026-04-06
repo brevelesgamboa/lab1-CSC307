b5112e05c009c3977660036d73c956d9978b812e
brevelesgamboa <bryanreveles3@gmail.com>
2026-04-06 10:49:55 -0700

    IA 2: My First REST API part 5 complete

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -38,10 +38,18 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
 
 app.get("/users", (req, res) => {
@@ -55,8 +63,14 @@ app.get("/users", (req, res) => {
   }
 });
 
-app.get("/users", (req, res) => {
-  res.send(users);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
